69da0c098279c8ee5f7b77143bb90ae9b8318c83
Iloba <scarlettsney@gmail.com>
2021-09-24 02:59:06 +0100

    commit

--- resources/js/Pages/Chat/inputMessage.vue
+++ resources/js/Pages/Chat/inputMessage.vue
@@ -1,13 +1,44 @@
 <template>
     <div class="relative h-10 m-1">
         <div style="border-top: 1px solid #e6e6e6" class="grid grid-col-6">
-            <input type="text" v-model="message" @keyup.enter="sendMessage()" name="" placeholder="Say Something">
+            <input type="text" v-model="message" @keyup.enter="sendMessage()" placeholder="Say Something" 
+            class="col-span-5 outline-none p-1"/>
+           
+            <button @click="sendMessage()" class="place-self-end bg-gray-500 hover:bg-blue-700 p-1 mt-1 rounded text-white">
+                Send Message
+            </button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-   props: ['room']
+   props: ['room'],
+   data: function(){
+       return{
+           message: ''
+       }
+   },
+   methods:{
+       sendMessage(){
+          //validate form
+          if(this.message = ''){
+              return;
+          }
+
+          axios.post('chat/room/' + this.room.id + 'message', {
+              message: this.message
+          }).then(res => {
+              //check if request was successful
+              if(res.status == 201){
+                  this.message = '';
+                  this.$emit('messageSent');
+              }
+          }).catch(error => {
+              console.log(error);
+          })
+
+       }
+   }
 }
 </script>
